df8c843fcc635b361acba478db2d4f0bfee1eb8e
Jerito-kun <xAg7V28ekMq9Na6>
2020-09-03 02:34:59 +0300

    Добавлены функции и декораторы

--- Homework-1/main.py
+++ Homework-1/main.py
@@ -1,3 +1,115 @@
-
 # Домашняя работа, первое задание
-print('Nothing here')
+
+"""
+1. Написать функцию, которая принимает N целых чисел и возвращает список квадратов эих чисел.
+Бонусом будет сделать keyword аргумент для выбора степени, в которую будут возводиться числа
+
+2. Написать функцию, которая на вход принимает список из целых чисел,
+и возвращает только чётные/нечётные/простые числа (выбор производится передачей дополнительного аргумента)
+
+3. Cоздать декоратор для замера времени выполнения функции
+"""
+
+from time import time
+from functools import wraps
+
+
+def run_timer(func):  # Способ без functools
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        func(*args, **kwargs)
+        wrapper.__name__ = func.__name__
+        wrapper.__doc__ = func.__doc__
+        print("%s --> выполнено за %f сек." % (wrapper.__name__, time() - start_time))
+    return wrapper
+
+
+def run_timer_wraps_mode(func):  # С использованием functools
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        func(*args, **kwargs)
+        print("%s --> выполнено за %f сек." % (wrapper.__name__, time() - start_time))
+    return wrapper
+
+
+# def trace(func):
+#     """
+#     Trace calls made to the decorated function.
+#     """
+#     @wraps(func)
+#         def inner(*args, **kwargs):
+#             func(*args, **kwargs)
+#     return inner
+
+
+
+#@trace
+def fibonacci(position):  # Каждое последующее число равно сумме двух предыдущих чисел последовательности Фибоначчи
+    print("--> fib(%d)" % position)
+    result = 1
+    if position > 1:
+        result = fibonacci(position - 1) + fibonacci(position - 2)
+    print("<-- fib(%d) == %d" % (position, result))
+    return result
+
+
+@run_timer_wraps_mode
+def multiple_func(*nums, count=2):  # Функция возведения в степень
+    powered_list = []
+    for num in nums:
+        powered_list.append(num ** count)
+    print(powered_list)
+
+
+def is_odd(number):  # Проверка на нечётные числа
+    if number == 0:
+        return False
+    if number % 2 != 0:
+        return True
+    return False
+
+
+def is_even(number):  # Проверка на чётные числа
+    if number % 2 != 0:
+        return False
+    return True
+
+
+def is_simple(number):  # Проверка на простоту числа
+    if number == 0:  # Ноль не является простым числом по определению
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+
+@run_timer
+def select_nums(numbers, selection_type="ODD"):  # Возвращение чисел по выбранному критерию
+    check_functions = {'ODD': is_odd, 'EVEN': is_even, 'SIMPLE': is_simple}
+    result = []
+    for number in numbers:
+        if check_functions[selection_type](number):
+            result.append(number)
+    print(result)
+
+
+def main():
+
+    print("Вызов функции возведения чисел в степень без указания степени (по умолчанию — в квадрат")
+    multiple_func(1, 3, 5)
+    print("Вызов функции возведения чисел в степень с указанием степени (в примере — в степень 10240")
+    multiple_func(1, 3, 5, count=10240)
+    print("Вывод списка только нечётных чисел для набора чисел 1, 2, 3, 4, 5, 6, 7")
+    select_nums([1, 2, 3, 4, 5, 6, 7], "ODD")
+    print("Вывод списка только чётных чисел для набора чисел 1, 2, 3, 4, 5, 6, 7")
+    select_nums([1, 2, 3, 4, 5, 6, 7], "EVEN")
+    print("Вывод списка только простых чисел для набора чисел 1, 2, 3, 4, 5, 6, 7")
+    select_nums([1, 2, 3, 4, 5, 6, 7], "SIMPLE")
+    print("Вывод числа Фибоначчи (№ 3 в последовательности)")
+    fibonacci(3)
+
+
+if __name__ == "__main__":
+    main()
