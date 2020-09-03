986d01857a4d040807052915035c9a72e1c21dc7
Jerito-kun <xAg7V28ekMq9Na6>
2020-09-04 00:41:04 +0300

    Добавлен декоратор для обработки вызова вложенных функций (на примере вычисления фисла Фибоначчи)

--- Homework-1/main.py
+++ Homework-1/main.py
@@ -7,50 +7,64 @@
 2. Написать функцию, которая на вход принимает список из целых чисел,
 и возвращает только чётные/нечётные/простые числа (выбор производится передачей дополнительного аргумента)
 
-3. Cоздать декоратор для замера времени выполнения функции
+3. Создать декоратор для замера времени выполнения функции
 """
 
 from time import time
 from functools import wraps
 
 
-def run_timer(func):  # Способ без functools
+def run_timer(func):  # Способ без использования дополнительных библиотек
     def wrapper(*args, **kwargs):
         start_time = time()
         func(*args, **kwargs)
         wrapper.__name__ = func.__name__
         wrapper.__doc__ = func.__doc__
         print("%s --> выполнено за %f сек." % (wrapper.__name__, time() - start_time))
+
     return wrapper
 
 
-def run_timer_wraps_mode(func):  # С использованием functools
+def run_timer_wraps_mode(func):  # С использованием дополнительной библиотеки
     @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time()
         func(*args, **kwargs)
         print("%s --> выполнено за %f сек." % (wrapper.__name__, time() - start_time))
+
     return wrapper
 
 
-# def trace(func):
-#     """
-#     Trace calls made to the decorated function.
-#     """
-#     @wraps(func)
-#         def inner(*args, **kwargs):
-#             func(*args, **kwargs)
-#     return inner
+def trace(symbols):
+    """
+    Trace calls made to the decorated function.
+    """
+
+    def _trace(func):
+        calls = [-1]
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            calls[0] += 1
+            function_data = f"{func.__name__}({','.join([str(arg) for arg in args])})"
+            print(symbols * calls[0], end="")
+            print(f"--> {function_data}")
+            result = func(*args, **kwargs)
+            print(symbols * calls[0], end="")
+            print(f"<-- {function_data} == {result}")
+            calls[0] -= 1
+            return result
+
+        return inner
+
+    return _trace
 
 
-
-#@trace
-def fibonacci(position):  # Каждое последующее число равно сумме двух предыдущих чисел последовательности Фибоначчи
-    print("--> fib(%d)" % position)
+@trace("____")
+def fib(position):  # Каждое последующее число равно сумме двух предыдущих чисел последовательности Фибоначчи
     result = 1
     if position > 1:
-        result = fibonacci(position - 1) + fibonacci(position - 2)
-    print("<-- fib(%d) == %d" % (position, result))
+        result = fib(position - 1) + fib(position - 2)
     return result
 
 
@@ -96,7 +110,6 @@ def select_nums(numbers, selection_type="ODD"):  # Возвращение чис
 
 
 def main():
-
     print("Вызов функции возведения чисел в степень без указания степени (по умолчанию — в квадрат")
     multiple_func(1, 3, 5)
     print("Вызов функции возведения чисел в степень с указанием степени (в примере — в степень 10240")
@@ -108,7 +121,7 @@ def main():
     print("Вывод списка только простых чисел для набора чисел 1, 2, 3, 4, 5, 6, 7")
     select_nums([1, 2, 3, 4, 5, 6, 7], "SIMPLE")
     print("Вывод числа Фибоначчи (№ 3 в последовательности)")
-    fibonacci(3)
+    fib(3)
 
 
 if __name__ == "__main__":
